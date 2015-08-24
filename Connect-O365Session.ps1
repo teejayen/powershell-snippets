02fb31ad3725f0d0f954aaec2740488d211f31b2
Timothy Neilen <tneilen@gmail.com>
2015-08-24 15:42:34 +1000

    Create Connect-O365Session.ps1

---
+++ Connect-O365Session.ps1
@@ -0,0 +1,3 @@
+ $LiveCred = Get-Credential
+ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+ Import-PSSession $Session
