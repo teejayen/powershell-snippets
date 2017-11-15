ad749367bae1c2605e4b8b18078b5f236d9123f9
Timothy Neilen <tneilen@gmail.com>
2017-11-16 09:19:36 +1000

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+function prompt
+{
+    $Host.UI.RawUI.WindowTitle = "PowerShell v" + (get-host).Version.Major + "." + (get-host).Version.Minor + " (" + $pwd.Provider.Name + ") " + $pwd.Path
+ 
+    # Am I admin?
+    if( (
+        New-Object Security.Principal.WindowsPrincipal (
+            [Security.Principal.WindowsIdentity]::GetCurrent())
+        ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+    {
+        $Host.UI.RawUI.WindowTitle = "[Admin] " + $Host.UI.RawUI.WindowTitle
+        Write-Host "[" -nonewline -foregroundcolor DarkRed
+        Write-Host "Admin" -nonewline -foregroundcolor Red
+        Write-Host "] " -nonewline -foregroundcolor DarkRed
+    }
+ 
+    return " "
+}
+
+Function Get-DinoPass {
+    $Password = Invoke-WebRequest -uri http://www.dinopass.com/password/strong
+    Set-Clipboard $Password.Content
+    Return $Password.Content
+}
+
+Clear-Host
+Set-ExecutionPolicy Bypass -Force
+$autoload="%UserProfile%\Documents\WindowsPowerShell\Scripts\autoload"
+Get-ChildItem "${autoload}\*.ps1" | %{.$_} 
+Write-Host "Custom PowerShell Environment Loaded" 
