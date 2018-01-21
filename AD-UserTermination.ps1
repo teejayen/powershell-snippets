a6b66791f125716c19ac9ac6f4cd2a8ae7a5be3d
Timothy Neilen <tneilen@gmail.com>
2018-01-22 09:56:04 +1000

    set description
    Added prompt for ticket number to set description of AD User Object, and more verbose output.

--- AD-UserTermination.ps1
+++ AD-UserTermination.ps1
@@ -1,10 +1,13 @@
 Import-Module ActiveDirectory
 $User = Read-Host -Prompt "Enter terminated username"
+$Ticket = Read-Host -Prompt "Enter ticket number for request"
 $Password = Invoke-WebRequest -uri http://www.dinopass.com/password/strong
+$TerminationDate = (Get-Date).AddMonths(6)
 $PrimaryGroupToken = (Get-ADGroup "Domain Users" -Properties PrimaryGroupToken).PrimaryGroupToken
+$UserDescription = "Terminated on $TerminationDate ($Ticket)"
 
 Set-ADAccountPassword $User -reset -newpassword (ConvertTo-SecureString -AsPlainText $Password -Force) 
-Set-AdAccountExpiration $User -datetime "(Get-Date).AddMonths(6)"
+Set-AdAccountExpiration $User -datetime $TerminationDate
 Get-ADUser $User -Properties PrimaryGroup,MemberOf | ForEach-Object {
 If ($_.PrimaryGroup -notmatch "Domain Users"){
     Set-ADUser -Identity $_ -Replace @{PrimaryGroupID = $PrimaryGroupToken} -Verbose
@@ -14,7 +17,8 @@ If ($_.memberof) {
                      Remove-ADPrincipalGroupMembership -Identity $_ -MemberOf $Group -Confirm:$false -Verbose
                   }
 }
-
+Set-ADUser $User -Description $UserDescription
+Write-Host "$User has been terminated!"
 Write-Output "Password has been set to $Password"
 Write-Output "User account will expire on (Get-Date).AddMonths(6)"
 Write-Output "User has been removed from $_.memberof"
