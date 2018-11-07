143ec2424193dea5809909a7b022a5b388dd9945
Timothy Neilen <tneilen@gmail.com>
2018-11-07 10:24:33 +1000

    formatting and output results to csv

--- Test-RDPPort.ps1
+++ Test-RDPPort.ps1
@@ -1,13 +1,27 @@
-$Customers = Import-CSV C:\temp\rdpcustomers.csv
-foreach ($Customer in $Customers) {
-Write-Host "Testing $($Customer.Name) - $($Customer.PublicIP) now..."
-$RDP = Test-NetConnection -ComputerName $($Customer.PublicIP) -Port 3389 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue 
-if($RDP.TcpTestSucceeded)
-{
+$Customers = Import-CSV C:\temp\rdpcustomers-3.csv
+
+foreach ($Customer in $Customers){
+    Write-Host "Testing $($Customer.Name) - $($Customer.PublicIP) now..."
+	$RDP = Test-NetConnection -ComputerName $($Customer.PublicIP) -Port 3389 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue 
+
+	if($RDP.TcpTestSucceeded){
     "Port 3389 is open"
-}
-else
-{
+	$Result = "Open"
+	}
+
+	else{
     "Port 3389 is filtered"
-}
+	}
+		
+	if($Result){
+		$ResultsExport = $null
+		$ResultsExport = [ordered]@{
+			Customer = $Customer.Name
+			PublicIP = $Customer.PublicIP
+			Result = $Result
+		}
+		
+		$ruleObject = New-Object PSObject -Property $ResultsExport
+		$ruleObject | Export-Csv C:\temp\rdpcustomersresult.csv -NoTypeInformation -Append
+	}
 }
