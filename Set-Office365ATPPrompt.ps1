3ba951efad110847c5829fdce09b434bb27c5f85
Timothy Neilen <tneilen@gmail.com>
2019-02-11 12:56:30 +1000

    initial commit

---
+++ Set-Office365ATPPrompt.ps1
@@ -0,0 +1,22 @@
+$Credentials = Get-Credential
+$SafeAttachmentsPolicyName = "aIT - Safe Attachments Policy"
+$SafeAttachmentsPolicyDescription = "aIT Safe Attachments Policy V1.0 - Block Delivery"
+$SafeAttachmentsRuleName = "All Recipients"
+$RedirectTo = "atpmailbox@answersit.com.u"
+$SafeLinksPolicyName = "aIT - Safe Links Policy"
+$SafeLinksPolicyDescription = "aIT Safe Links Policy V1.0"
+
+Connect-MsolService -Credential $credentials
+$cid = (Get-MsolPartnerContract | Out-GridView -PassThru).tenantid 
+$InitialDomain = Get-MsolDomain -TenantId $cid | Where-Object {$_.IsInitial -eq $true} 
+$DelegatedOrgURL = "https://outlook.office365.com/powershell-liveid?DelegatedOrg=" + $InitialDomain.Name 
+
+$EXODS = New-PSSession -ConnectionUri $DelegatedOrgURL -Credential $credentials -Authentication Basic -ConfigurationName Microsoft.Exchange -AllowRedirection
+Import-PSSession $EXODS -AllowClobber -CommandName Get-AcceptedDomain, New-SafeAttachmentPolicy, New-SafeAttachmentRule, New-SafeLinksPolicy, New-SafeLinksRule, Get-AtpPolicyForO365, Set-AtpPolicyForO365
+Set-AtpPolicyForO365 -Identity Default -EnableATPForSPOTeamsODB $true -EnableSafeLinksForClients $true -EnableSafeLinksForWebAccessCompanion $true -TrackClicks $true 
+New-SafeLinksPolicy -Name $safeLinksPolicyName -AdminDisplayName $safeLinksPolicyDescription -Enabled $true -DoNotTrackUserClicks $false -ScanUrls $true -EnableForInternalSenders $true
+$domains = Get-AcceptedDomain
+$domains = $domains.name -join ","
+New-SafeAttachmentPolicy -Name $safeAttachmentsPolicyName -Redirect $true -Enable $true -AdminDisplayName $safeAttachmentsPolicyDescription -Action Block ` -RedirectAddress $redirectTo -ActionOnError $false 
+New-SafeAttachmentRule -SafeAttachmentPolicy $safeAttachmentsPolicyName -RecipientDomains $domains -Name $safeAttachmentsRuleName
+Remove-PSSession $EXODS
