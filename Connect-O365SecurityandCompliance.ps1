460b1e3cd551bfedb0a6bae2ff1619c8f881a7c9
teejayen <timothy@neilen.com.au>
2020-06-11 08:34:14 +1000

    new snippet for security / compliance center connection

---
+++ Connect-O365SecurityandCompliance.ps1
@@ -0,0 +1,3 @@
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $UserCredential -AllowRedirection
+Import-PSSession $Session
